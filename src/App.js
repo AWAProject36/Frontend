c6dd76271bf08d29e6dcff5f94ea40e6215d2b26
jonoppe677 <joonas.erkkila98@gmail.com>
2021-11-29 12:38:03 +0200

    Create restaurant toiminto

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import Login from './components/Login';
 import CreateMenu from './components/CreateMenu';
 import './App.css';
 import { useState } from 'react';
+import CreateRestaurant from './components/CreateRestaurant';
 
 function App() {
-  const [buttonPopup1, setButtonPopup1] = useState(false);
   const [buttonPopup, setButtonPopup] = useState(false);
+  const [buttonPopup1, setButtonPopup1] = useState(false);
+  const [buttonPopup2, setButtonPopup2] = useState(false);
 
   return (
     <div className="App">
       <main>
+        <button onClick={() => setButtonPopup2(true)}>Create Restaurant</button>
         <button onClick={() => setButtonPopup1(true)}>Log in</button>
         <button onClick={() => setButtonPopup(true)}>Create menu</button>
       </main>
@@ -19,6 +22,9 @@ function App() {
         <CreateMenu trigger={buttonPopup} setTrigger={setButtonPopup}>
           <h2>Create menu</h2>
         </CreateMenu>
+        <CreateRestaurant trigger={buttonPopup2} setTrigger={setButtonPopup2}>
+        <h3>Create a new restaurant</h3>
+        </CreateRestaurant>
     </div>
   );
 }
