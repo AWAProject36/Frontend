97fbaee77a851c368cfe6c210d97930cff42cf5c
Kalle Perälä <peralakalle1@gmail.com>
2021-11-29 03:34:28 +0200

    API integration added.

--- src/App.js
+++ src/App.js
@@ -4,13 +4,23 @@ import { useState } from 'react';
 
 function App() {
   const [buttonPopup, setButtonPopup] = useState(false);
+  const [jwtToken, setJwtToken] = useState("");
+  function logToken(){
+    if(jwtToken === ""){
+    console.log("not logged in");
+    }
+    else{
+    console.log(jwtToken);
+    }
+  }
   
   return (
     <div className="App">
       <main>
         <button onClick={() => setButtonPopup(true)}>Log in</button>
+        <button onClick={ logToken }>Check token</button>
       </main>
-        <Login trigger={buttonPopup} setTrigger={setButtonPopup}>
+        <Login trigger={buttonPopup} setTrigger={setButtonPopup} setJwtToken={setJwtToken}>
           <h3>Log in</h3>
         </Login>
     </div>
--- src/components/login.js
+++ src/components/login.js
@@ -1,7 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './Login.module.css'
+const axios = require('axios');
+
 
 function Login(props) {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const loginSubmit = async () => {
+        try {
+          const result = await axios.post('https://voltti-app.herokuapp.com/login', null, {
+            auth: {
+              username: email,
+              password: password
+            }
+          })
+          props.setJwtToken(result.data.token);
+        } catch (error) {
+          console.error(error.message);
+        }
+      }
+
     return (props.trigger) ? (
         <div className={ styles.popup }>
             <div className={ styles.popupinner }>
@@ -10,12 +28,14 @@ function Login(props) {
                 { props.children }
                 <div classname="formgroup">
                     <label htmlFor="email">Email:</label>
-                    <input type ="email" name="email" id="email"/>
+                    <input type ="email" name="email" id="email" onChange = { (event) =>  setEmail(event.target.value) }/>
                 </div>
-                <div classname="formgroup">
+                <div className="formgroup">
                     <label htmlFor="password">Password:</label>
-                    <input type ="password" name="password" id="password"/>
+                    <input type ="password" name="password" id="password" onChange = { (event) =>  setPassword(event.target.value) }/>
                 </div>
+                <button onClick = { loginSubmit }
+                >Login</button>
             </div>
         </div>
     ) : "";
