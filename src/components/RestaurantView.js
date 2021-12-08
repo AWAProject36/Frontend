d66946c33b9235e9d0e88f5b3cb558bc41ecd7d1
atuppurainen <atuppura@gmail.com>
2021-12-08 17:46:22 +0200

    RestaurantView css

--- src/components/RestaurantView.js
+++ src/components/RestaurantView.js
@@ -1,26 +1,29 @@
 import React from 'react'
 import { useParams } from 'react-router'
-import styles from './RestaurantView.css'
 
 const RestaurantView = (props) => {
     const params = useParams()
 
     const data = props.restaurants.find(data => data.idrestaurant === parseInt(params.idrestaurant))
     console.log(data)
-    if(data == null) {
+    if (data == null) {
         return <div>No data</div>
     }
 
     return (
-        <div className={styles.vieWContainer}>
-            <img src={`/images/${data.img}`} alt={data.name} />
-            <div>
-                <div>{data.name}</div>
-                <div>{data.adress}</div>
-                <div>{data.type}</div>
-                <div>{data.pricelvl}</div>
-                <div>{data.opening}</div>
-                <div>{data.closing}</div>
+        <div className="viewContainer">
+            <div className="restaurantInfo">
+                <img src={`/images/${data.img}`} alt={data.name} />
+                <div className="restaurantData">
+                    <div>{data.name}</div>
+                    <div>{data.adress}</div>
+                    <div>{data.type}</div>
+                    <div>{data.pricelvl}</div>
+                    <div>{data.opening}</div>
+                    <div>{data.closing}</div>
+                </div>
+            </div>
+            <div className="foodMenu">
             </div>
         </div>
     )
